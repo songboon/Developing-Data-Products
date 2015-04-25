19e60ed49c26616ef784dddeb74ae570d5fe3d95
song boon <songboon@gmail.com>
2015-04-25 23:24:52 +0800

    version 2

---
+++ server.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  library(shiny)
+  
+  # Rely on the 'WorldPhones' dataset in the datasets
+  # package (which generally comes preloaded).
+  library(datasets)
+  
+  # Define a server for the Shiny app
+  shinyServer(function(input, output) {
+    
+    # Fill in the spot we created for a plot
+    output$phonePlot <- renderPlot({
+      
+      # Render a barplot
+      barplot(WorldPhones[,input$region]*1000, 
+              main=input$region,
+              ylab="Number of Telephones",
+              xlab="Year")
+    })
+  })
+})
